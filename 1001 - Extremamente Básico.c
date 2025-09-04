cdc76f47da1b5fbc68ce3ba5d7d2a4b7b95b2fa1
Gustavo Torquetti Cassiolato Silva <gustavo.tcassiolato@gmail.com>
2025-09-04 15:39:43 -0300

    Add files via upload

---
+++ 1001 - Extremamente Básico.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int A;
+    int B;
+    scanf("%i %i", &A, &B);
+    int X = A + B;
+    printf("X = %i\n", X);
+    return 0;
+}
---
+++ 1003 - Soma Simples.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int A;
+    int B;
+    scanf("%i %i", &A, &B);
+    int resposta = A + B;
+    printf("SOMA = %i\n", resposta);
+    return 0;
+}
